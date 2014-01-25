6e96f4428c5736e41c83c677d9e741be7443fe3d
vVfishVv <vmpiro111@yahoo.com>
2014-01-25 10:16:18 -0500

    Master
    mm 1/25/14

---
+++ Problem1.js
@@ -0,0 +1,30 @@
+<script type="text/javascript">
+<!--
+var number1, number2;
+var finalAnswer = 0;
+
+for (i = 0; i < 1000; i++)
+{
+number1 = i;
+//alert (i)
+answer = number1 % 3
+if (answer == 0)
+	 {
+// 				 alert("Yoooooooooo dat makes a Zero");
+					 finalAnswer = finalAnswer + number1;
+//				 alert(finalAnswer);
+		}
+else
+		{
+		answer = number1 % 5
+					 if (answer == 0)
+	 				 {
+					 finalAnswer = finalAnswer + number1;
+					 //				 alert(finalAnswer);					 
+					 }									 	 				
+			}	 
+//		alert("Does not compute Homie");
+}
+document.write(finalAnswer); 
+// -->
+</script>
